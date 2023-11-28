d022696714007dd98451ccac2b874da5c8ea39ef
“Saidarao” <glovisiontechnoservices@gmail.com>
2023-11-28 07:38:44 +0530

    changes in mainactivity

--- app/src/main/java/com/androindian/bletooth/MainActivity.kt
+++ app/src/main/java/com/androindian/bletooth/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.androindian.bletooth
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +33,20 @@ class MainActivity : AppCompatActivity() {
         button1?.setOnClickListener {
             if (!mBluetoothAdapter!!.isEnabled) {
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
             }
         }
